260a2c2db1f5c10a624e47e78d40a230c041bf02
GrafGeometr <exvita@yandex.ru>
2024-01-11 19:01:40 +0300

    Парсер для поля из массива Boolean

--- src/main/kotlin/logic/Board.kt
+++ src/main/kotlin/logic/Board.kt
@@ -1,6 +1,24 @@
 package org.example.logic
 
-class Board {
-    private val ships: List<Ship> = emptyList()
-    private val misses: List<Cell> = emptyList()
+class Board(
+    private var ships: List<Ship> = emptyList(),
+    var misses: List<Cell> = emptyList(),
+) {
+    private val size = 10
+
+    constructor(field: Array<Array<Boolean>>) : this() {
+
+        (4 downTo 1).forEach { shipSize ->
+            // horizontal ship
+            for (y in 0..<size) for (x in 0..<size - shipSize)
+                if ((0..<shipSize).all { field[x + it][y] }) {
+                    ships += Ship(x, y, x + shipSize - 1, y)
+                }
+            // vertical ship
+            for (x in 0..<size) for (y in 0..<size - shipSize)
+                if ((0..<shipSize).all { field[x][y + it] }) {
+                    ships += Ship(x, y, x, y + shipSize - 1)
+                }
+        }
+    }
 }
