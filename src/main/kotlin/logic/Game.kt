1e7b171e3f624ecedb74eecf432ba91e3b1a70b3
yazorinv <154721635+yazorinv@users.noreply.github.com>
2024-01-12 19:28:34 +0300

    Исправления для PR

--- src/main/kotlin/logic/Game.kt
+++ src/main/kotlin/logic/Game.kt
@@ -1,43 +1,31 @@
 package org.example.logic
 
-class Game(board1: Board = Board(), board2: Board = Board(), whoUserMove: Int = 0) {
+class Game(board1: Board = Board(), board2: Board = Board(), private var whoMoves: Int) {
     private var boards: MutableList<Board> = mutableListOf(board1, board2)
-    private var whoMove = whoUserMove // 0 or 1
     private var run = false
 
-    fun getWhoMove(): Int {
-        return whoMove
-    }
+    fun getWhoMove(): Int = whoMoves
 
-    fun gameRunning(): Boolean {
-        return run
-    }
+    fun gameRunning(): Boolean = run
 
-    fun isWin(user: Int): Boolean {
-        return boards[user xor 1].gameIsLost()
-    }
+    fun isWin(user: Int): Boolean = boards[user xor 1].gameIsLost()
 
-    fun isLose(user: Int): Boolean {
-        return boards[user].gameIsLost()
-    }
+    fun isLose(user: Int): Boolean = boards[user].gameIsLost()
 
     // Это значит, что текущий игрок стреляет в ячейку cell, которая, конечно же, попадает по полю противника
-    fun shotUser(cell: Cell, user: Int) {
-        if (user != whoMove)
+    fun makeShot(cell: Cell, user: Int) {
+        if (user != whoMoves)
             return
         boards[user xor 1].shot(cell)
         if (isLose(user))
             run = false
+        whoMoves = whoMoves xor 1
     }
 
-    fun getBoard(user: Int, detail: Boolean = false): String {
-        if (detail)
-            return boards[user].showStringDetailed()
-        else
-            return boards[user].showString()
+    fun getBoard(user: Int, detail: Boolean = false): String = when {
+        detail -> boards[user].showStringDetailed()
+        else -> boards[user].showString()
     }
 
-    fun startGame() {
-        run = true
-    }
+    fun startGame(): Boolean = run
 }
